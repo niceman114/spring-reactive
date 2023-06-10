aed254b361a8a88582fe1dee87adcdb6b538ee3b
niceman114 <niceman114@gmail.com>
2023-06-10 13:57:57 +0900

    Add two endpoints for Server Sent Events

--- src/main/java/com/example/springreactive/web/controller/PostController.java
+++ src/main/java/com/example/springreactive/web/controller/PostController.java
@@ -4,9 +4,13 @@ import com.example.springreactive.domain.entity.Post;
 import com.example.springreactive.domain.exception.PostNotFoundException;
 import com.example.springreactive.domain.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +22,19 @@ public class PostController {
     @GetMapping(value = "")
     public Flux<Post> all() {
         return this.postRepository.findAll().log();
+    }
+
+    @GetMapping(value = "/sse/v1", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Post> sse() {
+        return this.postRepository.findAll().delayElements(Duration.ofSeconds(1)).repeat().log();
+    }
+
+    @GetMapping(value = "/sse/v2", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Post> sse2() {
+        return Flux
+                .zip(Flux.interval(Duration.ofSeconds(1)), this.postRepository.findAll().repeat())
+                .map(Tuple2::getT2)
+                .log();
     }
 
     @GetMapping(value = "/{id}")
